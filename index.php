6c1138aabb2e7d004243d93f1acf6e92d868a7d7
Antonio Anerão <anerao.junior@gmail.com>
2022-05-08 16:54:40 -0500

    Adicionando autoload

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
-$query = require 'core/bootstrap.php';
+require 'vendor/autoload.php';
 
-
+ require 'core/bootstrap.php';
 
 require Router::load('routes.php')->direct(Request::uri(), Request::method());
